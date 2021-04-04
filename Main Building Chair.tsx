9049a8438f1fc2cb241cc837ec133673f70f5cc8
AwesomeKalin <47223207+AwesomeKalin@users.noreply.github.com>
2021-04-04 17:13:55 +0100

    Started Main Building Inside

---
+++ Main Building Chair.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="2021.01.13" name="Main Building Chair" tilewidth="32" tileheight="32" tilecount="18" columns="6">
+ <image source="Main Building Chair.png" width="196" height="103"/>
+</tileset>
